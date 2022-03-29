d2c2bcb4cde119edc6bba1f32e136764bb9c29f0
souad988 <59707859+souad988@users.noreply.github.com>
2022-03-29 09:18:06 +0000

    Add CHECK_STATUS action

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -2,9 +2,12 @@ export const categoriesState = [];
 
 const ADD_CATEGORY = 'ADD_CATEGORY';
 const REMOVE_CATEGORY = 'REMOVE_CATEGORY';
+const CHECK_STATUS = 'CHECK_STATUS'
 
-export const categoriesReducer = (categoriesState, action) => {
+export const categoriesReducer = (categoriesState = [], action) => {
   switch (action.type) {
+    case CHECK_STATUS:
+      return 'Under Construction';
     case ADD_CATEGORY:
       return [...categoriesState, action.category];
     case REMOVE_CATEGORY:
@@ -13,6 +16,10 @@ export const categoriesReducer = (categoriesState, action) => {
       return categoriesState;
   }
 };
+
+export function checkStatus(){
+  return {type:CHECK_STATUS}
+}
 
 export function addCategory(category) {
   return { type: ADD_CATEGORY, category };
