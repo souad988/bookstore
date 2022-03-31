c7540c344602742fe5658ca6d3bab746b9f0b91d
souad988 <59707859+souad988@users.noreply.github.com>
2022-03-31 11:25:36 +0000

    Add checkStatus dispatcher to category component

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import {useDispatch, useSelector} from 'react-redux'
+import {checkStatus} from  '../redux/categories/categories'
+
 
 function Categories() {
+  const categories = useSelector(state=>state.categories)
+  const dispatch=useDispatch()
   return (
     <div>
-      <button type="button" className="blue_btn">Check status</button>
+      <button type="button" className="blue_btn" onClick={()=>dispatch(checkStatus())} >Check status</button>
+      <h2>{categories}</h2>
     </div>
   );
 }
