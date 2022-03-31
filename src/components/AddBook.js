77b648aefb80a6a9b2937fe844f1e9ecca65c4d1
souad988 <59707859+souad988@users.noreply.github.com>
2022-03-31 09:52:24 +0000

    Install UUID

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React,{useState} from 'react';
+import {useDispatcher} from 'react-redux'
+import {addBook} from  '../redux/books/books'
 
 function AddBook() {
+  const dispatch=useDispatcher();
+  const [book,useBook]=useState(
+    {
+      id: 1,
+      title: 'The Yellow Meads of Asphodel',
+      author: 'H. E. Bates',
+      category: 'action',
+      progress: '64',
+      chapter: 'chapter 17',
+    }
+  );
+
   return (
     <div className="addBook_container">
       <h2>ADD NEW BOOK</h2>
@@ -12,7 +26,7 @@ function AddBook() {
           <option value="drama">drama</option>
           <option value="classic">classic</option>
         </select>
-        <button type="button" className="blue_btn">Add Book</button>
+        <button type="button" onClick={()=>dispatch(addBook())} className="blue_btn">Add Book</button>
 
       </form>
     </div>
